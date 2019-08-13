f639f6da9fa6bf6231540fdc4408aebe0bfb2429
Noor Al Darraji <077068@yahoo.com>
2019-08-13 15:21:27 -0500

    Step :one: done

---
+++ src/reducers/Reducer.jsx
@@ -0,0 +1,13 @@
+export const initialState = {
+
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+
+}
+
+export const Reducer = (state, action) => {
+
+    return state;
+
+}
